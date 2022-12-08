3180f27f57ff2283d633c792bd83fb1fe057b5d5
Adithya KP <adithyakpb@gmail.com>
2022-12-08 18:40:40 +0530

    changes2

--- src/Car.java
+++ src/Car.java
@@ -9,5 +9,40 @@ public class Car {
         this.make = make;
         this.speed = speed;
     }
-    getModel
+    public int getModel()
+    {
+        return model;
+    }
+    public String getMake()
+    {
+        return make;
+    }
+    public int getSpeed()
+    {
+        return speed;
+    }
+
+    public void accelerate()
+    {
+        speed = speed + 5;
+    }
+    public void brake()
+    {
+        speed = speed - 5;
+    }
+
+    public static void main(String[] args) {
+        Car rari = new Car(1990, "F40", 120);
+        for (int i = 0; i < 5; i++) {
+            rari.accelerate();
+        }      
+        
+        System.out.println("Speed is " + rari.getSpeed());
+
+        for (int i = 0; i < 5; i++) {
+            rari.brake();
+        }
+        
+        System.out.println("Speed is " + rari.getSpeed());
+    }
 }

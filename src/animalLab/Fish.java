7e17be13454acf171bfc23646bb7b4cf21aba953
Adithya KP <adithyakpb@gmail.com>
2023-01-09 00:07:16 +0530

    cie 1 edited programs

---
+++ src/animalLab/Fish.java
@@ -0,0 +1,25 @@
+package animalLab;
+
+public class Fish extends Animal implements Pet{
+    private String name;
+    Fish(){
+        
+        super(0);
+    
+    }
+    void setName(String name){
+        this.name = name;
+    }
+    public String getName(){
+        return this.name;
+    }
+    void walk(){
+        System.out.println("Fish can't walk bro");
+    }
+    public void play(){
+        System.out.println(this.name + " is playing");
+    }
+    public void eat(){
+        System.out.println(this.name + "is eating");
+    }
+}
